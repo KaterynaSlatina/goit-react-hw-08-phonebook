cb15a99811b827a45b42787216c11cbb2c412d22
KaterynaSlatina <131613660+KaterynaSlatina@users.noreply.github.com>
2024-01-21 19:59:58 +0000

    Deploying to gh-pages from @ KaterynaSlatina/goit-react-hw-08-phonebook@4fd35bad5c0e99cd7f89f3f74e6db32ec5e6d72d 🚀

---
+++ static/js/910.08472944.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[910],{2910:function(e,t,r){r.r(t);var a=r(3329);t.default=function(){return(0,a.jsx)("div",{children:"Contacts"})}}}]);
+//# sourceMappingURL=910.08472944.chunk.js.map
