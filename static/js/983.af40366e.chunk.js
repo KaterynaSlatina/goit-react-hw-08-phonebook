0ba5532463a9c11b80fd6e9861e6383b8e2b3207
KaterynaSlatina <131613660+KaterynaSlatina@users.noreply.github.com>
2024-01-24 11:47:06 +0000

    Deploying to gh-pages from @ KaterynaSlatina/goit-react-hw-08-phonebook@5885df4cd38a59d1b25e78ebb57747d951d8f0d7 🚀

---
+++ static/js/983.af40366e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[983],{3983:function(e,t,r){r.r(t);var n=r(3329);t.default=function(){return(0,n.jsx)(n.Fragment,{children:(0,n.jsx)("h1",{children:"Welcome to the PhoneBook!"})})}}}]);
+//# sourceMappingURL=983.af40366e.chunk.js.map
