2561f3e373d3f0a259154a80bcfabce4c2c97d09
KaterynaSlatina <131613660+KaterynaSlatina@users.noreply.github.com>
2024-01-25 16:47:24 +0200

    add contacts

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,24 +1,13 @@
 import { FormContact } from 'components/ContactForm/ContactForm';
 import { ContactList } from 'components/ContactList/ContactList';
 import { Filter } from 'components/Filter/Filter';
-import { useSelector } from 'react-redux';
-import { selectVisibleContacts } from '../redux/contacts/selectors';
-import { ContactContainer } from 'components/ContactList/ContactList.styled';
 
 const Contacts = () => {
-  const contacts = useSelector(selectVisibleContacts);
-
   return (
     <div>
       <FormContact />
       <Filter />
-      {contacts.length > 0 ? (
-        <ContactList />
-      ) : (
-        <ContactContainer>
-          <p>No contacts</p>
-        </ContactContainer>
-      )}
+      <ContactList />
     </div>
   );
 };
